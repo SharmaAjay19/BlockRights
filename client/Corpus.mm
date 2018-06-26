be47f44633a6bc9e9d71e6f85e0372ae61ebc9a6
Ajay Sharma <ajsharm@microsoft.com>
2018-06-26 07:33:13 +0530

    Better visualization

--- client/Corpus.mm
+++ client/Corpus.mm
@@ -1,5 +1,12 @@
 %%MatrixMarket matrix coordinate real general
-2 3 3                                             
+4 5 10                                            
 1 1 1
+1 2 1
+2 1 1
 2 2 1
-2 3 1
+3 2 1
+3 3 1
+3 4 1
+4 2 1
+4 4 1
+4 5 1
